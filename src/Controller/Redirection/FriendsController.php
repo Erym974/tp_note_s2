2eb975fb462bb8edaa000259502c47057259d516
Erym974 <remyrichard974@live.fr>
2023-12-23 02:10:39 +0400

    Fix navbar

--- src/Controller/Redirection/FriendsController.php
+++ src/Controller/Redirection/FriendsController.php
@@ -8,7 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class FriendsController extends AbstractController 
 {
     
